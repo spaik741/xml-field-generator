6b6bdc9788acf3e25a6411b7a814f86f35bacfd7
bryahchin.mihail <bryahchin.mihail@otr.ru>
2022-03-23 10:16:57 +0300

    Initial commit

---
+++ src/main/java/com/otr/xml/XmlFieldGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.otr.xml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlFieldGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmlFieldGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/otr/xml/XmlFieldGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otr.xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlFieldGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
